acfa1c7b6205c841baa1d01f866fbe3d5684de27
Bandara-VKJ <it22291714@my.sliit.lk>
2026-03-24 13:34:43 +0530

    HH-14 Login page & Welcome page

--- HopeHubApp/FE/app/(tabs)/_layout.tsx
+++ HopeHubApp/FE/app/(tabs)/_layout.tsx
@@ -1,12 +1,13 @@
 import { Tabs } from "expo-router";
 
 export default function Layout() {
+  
   return (
     <Tabs>
-      <Tabs.Screen name="Home" options={{ title: "Home" }} />
-      <Tabs.Screen name="dashboard" />
-      <Tabs.Screen name="support" />
-      <Tabs.Screen name="lifebuild" options={{ title: "Life Build" }} />
+      <Tabs.Screen name="Home/home" options={{ title: "Home" }} />
+      <Tabs.Screen name="Dashboard/dashboard"  options={{ title: "Dashboard" }} />
+      <Tabs.Screen name="Support/support" options={{ title: "Support" }} />
+      <Tabs.Screen name="Lifebuild/lifebuild" options={{ title: "Life Build" }} />
     </Tabs>
   );
 }
