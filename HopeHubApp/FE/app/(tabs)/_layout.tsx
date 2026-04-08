95e226ffa209e354d966b5847a09a9728932c3ae
Bandara-VKJ <it22291714@my.sliit.lk>
2026-04-08 22:37:03 +0530

    HH-24 Add icons to nav bar

--- HopeHubApp/FE/app/(tabs)/_layout.tsx
+++ HopeHubApp/FE/app/(tabs)/_layout.tsx
@@ -1,13 +1,15 @@
 import { Tabs } from "expo-router";
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Layout() {
   
   return (
-    <Tabs>
-      <Tabs.Screen name="Home/home" options={{ title: "Home" }} />
-      <Tabs.Screen name="Dashboard/dashboard"  options={{ title: "Dashboard" }} />
-      <Tabs.Screen name="Support/support" options={{ title: "Support" }} />
-      <Tabs.Screen name="Lifebuild/lifebuild" options={{ title: "Life Build" }} />
+    <Tabs screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+      <Tabs.Screen name="Home/home" options={{ tabBarIcon: ({ color, size, focused }) => ( <Ionicons name={focused ? "home" : "home-outline"} size={size} color={color} /> ), }} />
+      <Tabs.Screen name="Dashboard/dashboard"  options={{ tabBarIcon: ({ color, size, focused }) => (<Ionicons name={focused ?  "grid" : "grid-outline"} size={size} color={color}/>),}}/>
+      <Tabs.Screen name="Support/support" options={{ tabBarIcon: ({ color, size, focused }) => (<Ionicons name={focused ? "chatbubbles" : "chatbubbles-outline"} size={size} color={color}/>),}}/>
+      <Tabs.Screen name="Lifebuild/lifebuild" options={{ tabBarIcon: ({ color, size, focused }) => (<Ionicons name={focused ? "trophy" : "trophy-outline"} size={size} color={color}/>),}} />
+      <Tabs.Screen name="Profile/profile" options={{ tabBarIcon: ({ color, size, focused }) => ( <Ionicons name={focused ? "person" : "person-outline"} size={size} color={color} />),}}/>
     </Tabs>
   );
 }
