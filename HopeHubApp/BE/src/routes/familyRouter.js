9972129e7bafbb09bd90a2f5a0a1e421c7a6adc4
Bandara-VKJ <it22291714@my.sliit.lk>
2026-07-28 21:55:43 +0530

    HH-69 Family member login

--- HopeHubApp/BE/src/routes/familyRouter.js
+++ HopeHubApp/BE/src/routes/familyRouter.js
@@ -1,10 +1,13 @@
 import express from "express";
-import {sendInvite, acceptInvite, familyMemberReg }  from "../controllers/familyController.js";
+import {sendInvite, acceptInvite, familyMemberReg, familyMemberLogin, getMyFamilyProfile }  from "../controllers/familyController.js";
+import {authenticateFamilyMember} from "../middlewares/authenticateFamilyMember.js"
 
 const router = express.Router()
 
 router.post("/invite", sendInvite);
 router.get("/accept", acceptInvite);
 router.post("/accept", familyMemberReg);
+router.post("/login", familyMemberLogin);
+router.get("/me", authenticateFamilyMember , getMyFamilyProfile)
 
 export default router;
